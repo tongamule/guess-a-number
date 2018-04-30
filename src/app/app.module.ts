7b081cc456494dad40292e2e41693c51d41c2ba4
Gaston Mule <gasmul@gmail.com>
2018-04-30 19:49:30 -0300

    add method to create number form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GuessANumberComponent } from './guess-a-number/guess-a-number.component';
 import { ProvideANumberComponent } from './provide-a-number/provide-a-number.component';
@@ -10,7 +10,7 @@ import { NumberHelperService } from './services';
 
 @NgModule({
   declarations: [AppComponent, GuessANumberComponent, ProvideANumberComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [NumberHelperService],
   bootstrap: [AppComponent]
 })
--- src/app/services/number-helper.service.ts
+++ src/app/services/number-helper.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Injectable()
 export class NumberHelperService {
+  constructor(private _fb: FormBuilder) {}
 
-  constructor() { }
-
+  getNumberForm() {
+    return this._fb.group({
+      number: [null, [Validators.required, Validators.min(1), Validators.max(100)]]
+    });
+  }
 }
