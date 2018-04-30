7fb907a7e2eff576d87f17d307869841b11bc11b
Gaston Mule <gasmul@gmail.com>
2018-04-30 20:50:38 -0300

    change number to higher or lower

--- src/app/provide-a-number/provide-a-number.component.ts
+++ src/app/provide-a-number/provide-a-number.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NumberHelperService, GuessResult } from '../services';
 
 @Component({
   selector: 'app-provide-a-number',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./provide-a-number.component.scss']
 })
 export class ProvideANumberComponent implements OnInit {
+  newGuess: number;
+  lastGuess: GuessResult;
+  constructor(private _numberHelper: NumberHelperService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  guessANumber() {
+    this.newGuess = this._numberHelper.getRandomNumber();
+    return this.newGuess;
   }
 
+  isEqual() {
+    alert('¡Gracias por jugar!');
+  }
+
+  goHigher() {
+    debugger;
+    const newNumber = this._numberHelper.getRandomNumber(this.newGuess + 1, 100);
+    this.validateNumber(newNumber);
+  }
+
+  goLower() {
+    debugger;
+    const newNumber = this._numberHelper.getRandomNumber(1, this.newGuess - 1);
+    this.validateNumber(newNumber);
+  }
+
+  private validateNumber(val: number) {
+    const isValid = val >= 1 || val <= 100;
+    if (!isValid) {
+      alert('Recuerda: el número debe ser entre 1 y 100');
+    } else {
+      this.newGuess = val;
+    }
+    return isValid;
+  }
 }
--- src/app/services/number-helper.service.ts
+++ src/app/services/number-helper.service.ts
@@ -16,8 +16,8 @@ export class NumberHelperService {
     });
   }
 
-  getRandomNumber() {
-    return Math.floor(Math.random() * 100 + 1);
+  getRandomNumber(min: number = 1, max: number = 100) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   checkNumberWithGuess(targetNumber: number, guessNumber: number): GuessResult {
